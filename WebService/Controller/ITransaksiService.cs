bf63bda352d462643f07c261dacc9dd7adec620d
Fathony Teguh Irawan <fathony.teguh@gmail.com>
2017-01-09 22:38:05 +0700

    latest update

--- WebService/Controller/ITransaksiService.cs
+++ WebService/Controller/ITransaksiService.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 using WebService.Model;
 
 namespace WebService.Controller
